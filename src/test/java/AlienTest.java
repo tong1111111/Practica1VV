5b238514aeb5ad30d67f6f31aa4ed259be7bef13
tongg <tong.ge@alumnos.upm.es>
2025-11-14 17:52:51 +0100

    Modificado los test del metodo initAlien(int x, int y) y act(int direction)

--- src/test/java/AlienTest.java
+++ src/test/java/AlienTest.java
@@ -44,72 +44,8 @@ public class AlienTest {
 
     @Test
     void testinitAlien_Caso2() {
-        int x = -10;
-        int y = 100;
-        Alien a = new Alien(x, y);
-
-        try {
-            Method metodo = a.getClass().getDeclaredMethod("initAlien", int.class, int.class);
-            metodo.setAccessible(true);
-            metodo.invoke(a, x,y);
-
-            Field valx = Sprite.class.getDeclaredField("x");
-            valx.setAccessible(true);
-            int valorx = (int) valx.get(a);
-
-            Field valy = Sprite.class.getDeclaredField("y");
-            valy.setAccessible(true);
-            int valory = (int) valy.get(a);
-
-            assertTrue((valorx == 0) && (valory == 100));
-
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    void testinitAlien_Caso3() {
-        int x = 359;
-        int y = 100;
-        Alien a = new Alien(x, y);
-
-        try {
-            Method metodo = a.getClass().getDeclaredMethod("initAlien", int.class, int.class);
-            metodo.setAccessible(true);
-            metodo.invoke(a, x,y);
-
-            Field valx = Sprite.class.getDeclaredField("x");
-            valx.setAccessible(true);
-            int valorx = (int) valx.get(a);
-
-            Field valy = Sprite.class.getDeclaredField("y");
-            valy.setAccessible(true);
-            int valory = (int) valy.get(a);
-
-            assertTrue((valorx == 358) && (valory == 100));
-
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    void testinitAlien_Caso4() {
         int x = 100;
-        int y = -10;
+        int y = -1;
         Alien a = new Alien(x, y);
 
         try {
@@ -139,7 +75,7 @@ public class AlienTest {
     }
 
     @Test
-    void testinitAlien_Caso5() {
+    void testinitAlien_Caso3() {
         int x = 100;
         int y = 351;
         Alien a = new Alien(x, y);
@@ -157,7 +93,71 @@ public class AlienTest {
             valy.setAccessible(true);
             int valory = (int) valy.get(a);
 
-            assertTrue((valorx == 100) && (valory == 350));
+            assertTrue((valorx == 100) && (valory == 351));
+
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testinitAlien_Caso4() {
+        int x = -1;
+        int y = 100;
+        Alien a = new Alien(x, y);
+
+        try {
+            Method metodo = a.getClass().getDeclaredMethod("initAlien", int.class, int.class);
+            metodo.setAccessible(true);
+            metodo.invoke(a, x,y);
+
+            Field valx = Sprite.class.getDeclaredField("x");
+            valx.setAccessible(true);
+            int valorx = (int) valx.get(a);
+
+            Field valy = Sprite.class.getDeclaredField("y");
+            valy.setAccessible(true);
+            int valory = (int) valy.get(a);
+
+            assertTrue((valorx == -1) && (valory == 100));
+
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testinitAlien_Caso5() {
+        int x = 359;
+        int y = 100;
+        Alien a = new Alien(x, y);
+
+        try {
+            Method metodo = a.getClass().getDeclaredMethod("initAlien", int.class, int.class);
+            metodo.setAccessible(true);
+            metodo.invoke(a, x,y);
+
+            Field valx = Sprite.class.getDeclaredField("x");
+            valx.setAccessible(true);
+            int valorx = (int) valx.get(a);
+
+            Field valy = Sprite.class.getDeclaredField("y");
+            valy.setAccessible(true);
+            int valory = (int) valy.get(a);
+
+            assertTrue((valorx == 359) && (valory == 100));
 
         } catch (NoSuchMethodException e) {
             throw new RuntimeException(e);
@@ -172,10 +172,10 @@ public class AlienTest {
 
     @Test
     void testAct_Caso1(){
-        int direccion = -3;
-        int x = 3;
+        int direccion = -1;
+        int x = 100;
         Alien a = new Alien(x,10);
         a.act(direccion);
-        assertEquals(0,a.getX());
+        assertEquals(101,a.getX());
     }
 }
